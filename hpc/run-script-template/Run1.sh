52fc4059d2c64c46b66f2bb8060ebc557dd46d3f
s204109 <s204109@student.dtu.dk>
2023-06-04 22:56:51 +0200

    testing hpc template

--- hpc/run-script-template/Run1.sh
+++ hpc/run-script-template/Run1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#BSUB -J Run1
-#BSUB -o ../runs-ignore/Run1_%J.out
-#BSUB -e ../runs-ignore/Run1_%J.err
+#BSUB -J pre_Sleep_Epilepsy
+#BSUB -o hpc/runs/Run1_%J.out
+#BSUB -e hpc/runs/Run1_%J.err
 #BSUB -q hpc
 #BSUB -W 2
 #BSUB -R "rusage[mem=512MB]"
 #BSUP -n 4
 #BSUB -N
 
-# all  BSUB option comments should be above this line!
+# since all commands are from xterm's cd,
+# remember to place cd in git folder: "ProjectWork2023"
 
-# execute our command
-python ../../code/main.py --training_mode pre_train --pretrain_dataset SleepEEG --target_dataset Epileps --subset False
+python code/Test.py
