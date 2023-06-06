4d6ac1533555a51656ffeffc03fa710d07ae1923
s204109 <s204109@student.dtu.dk>
2023-06-06 12:23:38 +0200

    hpc, requirements

--- hpc/run-scripts/OngThinLinc.sh
+++ hpc/run-scripts/OngThinLinc.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-#BSUB -J Run3_pre_Sleep_Epilepsy
-#BSUB -o hpc/runs/Run3_%J.out
-#BSUB -e hpc/runs/Run3_%J.err
+#BSUB -J pre_Sleep_Epilepsy_subNot
+#BSUB -o hpc/runs/%J.out
+#BSUB -e hpc/runs/J.err
 
 # gpu
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 02:00
-#BSUB -R "rusage[mem=8G] span[hosts=1]"
+
+# runtime
+#BSUB -W 20:00
+
+# specs
+#BSUB -R "rusage[mem=16G] span[hosts=1]"
 #BSUB -n 4
+
+# mail when done
 #BSUB -N
 
 # since all commands are from xterm's cd,
-# remember to place cd in git folder: "ProjectWork2023"
+# remember to place xterm cd in git folder: "ProjectWork2023"
 
-source environments/PW_env/bin/activate
+source hpc/environments/PW_env/bin/activate
 
-python code/main.py --training_mode pre_train --pretrain_dataset SleepEEG --target_dataset Epilepsy --subset True
+python code/main.py --training_mode pre_train --pretrain_dataset SleepEEG --target_dataset Epilepsy --subset False
+# SleepEEG -> Epilepsy
+# FD-A -> FD-B
+# ECG -> EMG
+# HAR -> Gesture
