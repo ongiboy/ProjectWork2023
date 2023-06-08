dcbc82e07b3fb8608e6802b100fcafe11c5b393d
s204109 <s204109@student.dtu.dk>
2023-06-08 10:35:17 +0200

    hpc is working!?

--- hpc/run-scripts/Ong.sh
+++ hpc/run-scripts/Ong.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#BSUB -J pre_Sleep_Epi_subYes
+#BSUB -J pre_Sleep_Epi_subNot
 #BSUB -o hpc/runs/Run_%J.out.txt
 #BSUB -e hpc/runs/Run_%J.err.txt
 
 # gpu
-#BSUB -q gpua10
+#BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
 
 # runtime
 #BSUB -W 6:00
 
 # specs
-#BSUB -R "rusage[mem=10GB] span[hosts=1]"
-#BSUB -n 4
+#BSUB -R "rusage[mem=12GB] span[hosts=1]"
+#BSUB -n 2
 
 # mail when done
 #BSUB -N
@@ -23,10 +23,10 @@
 source hpc/environments/PW_env/bin/activate
 
 # SleepEEG -> Epilepsy
-python code/main.py --training_mode pre_train --pretrain_dataset SleepEEG --target_dataset Epilepsy --subset True
+python code/main.py --training_mode pre_train --pretrain_dataset SleepEEG --target_dataset Epilepsy --subset False --device cuda
 # FD_A -> FD_B
-# python code/main.py --training_mode pre_train --pretrain_dataset FD-A --target_dataset FD-B --subset False
-# ECG -> EMG
-# python code/main.py --training_mode pre_train --pretrain_dataset ECG --target_dataset EMG --subset False
+# python code/main.py --training_mode pre_train --pretrain_dataset FD-A --target_dataset FD-B --subset True --device cuda
+# ECG -> EMG (>8GB pre)
+# python code/main.py --training_mode pre_train --pretrain_dataset ECG --target_dataset EMG --subset False --device cuda
 # HAR -> Gesture
-# python code/main.py --training_mode pre_train --pretrain_dataset HAR --target_dataset Gesture --subset False
+# python code/main.py --training_mode pre_train --pretrain_dataset HAR --target_dataset Gesture --subset False --device cuda
