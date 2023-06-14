502177fcd77e0bfdc3db5fe3404433ca69bd0a59
s214601 <s214601@student.dtu.dk>
2023-06-14 23:46:25 +0200

    data_p WITHOUT leak between train and test, and PCA update
    Signed-off-by: s214601 <s214601@student.dtu.dk>

--- code/PCA.py
+++ code/PCA.py
@@ -11,20 +11,20 @@ def PCA_embeddings(embeddings): # input numpy arrays, shape (x,2)
 
 if __name__ == "__main__":
     modes = ["pretraining", "finetuning"]
-    mode = modes[0]
+    mode = modes[1]
 
     #labels only needed if mode == finetune
     z_t = np.load(f"code\\PCA_embeddings\\{mode}\\z_t.npy")
-    z_t_aug = np.load(f"code\\PCA_embeddings\\{mode}\\z_t_aug.npy")
+    #z_t_aug = np.load(f"code\\PCA_embeddings\\{mode}\\z_t_aug.npy")
     z_f = np.load(f"code\\PCA_embeddings\\{mode}\\z_f.npy")
-    z_f_aug = np.load(f"code\\PCA_embeddings\\{mode}\\z_f_aug.npy")
+    #z_f_aug = np.load(f"code\\PCA_embeddings\\{mode}\\z_f_aug.npy")
 
-    embeddings = np.vstack((z_t, z_t_aug, z_f, z_f_aug))
+    embeddings = np.vstack((z_t, z_f))  # z_t_aug z_f_aug
     embeddings_pca = PCA_embeddings(embeddings)
 
     plt.figure(figsize=(8, 6))
     if mode == "finetuning":
-        labels = list(np.load(f"code\\PCA_embeddings\\{mode}\\labels.npy")) * 4
+        labels = list(np.load(f"code\\PCA_embeddings\\{mode}\\labels.npy")) * 2
         unique_labels = np.unique(labels)
         for label in unique_labels:
             mask = labels == label
--- code/data_processing/data_processing_depression.py
+++ code/data_processing/data_processing_depression.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 import torch
 from process_functions import check_consecutive_repeats
 
+np.random.seed(12)
 path = os.getcwd()
 cond = f'{path}\datasets\Depression\data\condition'
 cont = f'{path}\datasets\Depression\data\control'
@@ -28,102 +29,108 @@ for j in range(len(Depression_datasets)):
 
     # defining data
     segment_length = 60*24 * num_days
-    segmented_data = []
-    labels = []
+    segmented_data_train = []
+    segmented_data_test = []
+    labels_train = []
+    labels_test = []
 
     # Load and append data from the control and condition folder
     folders = [cont, cond]
     for folder, label in zip(folders, [0, 1]):
         files = os.listdir(folder)
-        for file in files:
-            file_path = os.path.join(folder, file)
-            DATA = pd.read_csv(file_path)
-            start_index = 0
-            
-            ## Finding the first 00:00:00 timestamp
-            for i in range(len(DATA["timestamp"])):
-                if DATA["timestamp"][i][11:19] == '00:00:00':
-                    start_index = i
-                    break
-            data = DATA["activity"][start_index:]
+        np.random.shuffle(files)
+        train_files, test_files = train_test_split(files, test_size=0.75, random_state=12)
+
+        file_types = [train_files, test_files]
+
+        for file_type in file_types:
+
+            for file in file_type:
+                file_path = os.path.join(folder, file)
+                DATA = pd.read_csv(file_path)
+                start_index = 0
+
+                ## Finding the first 00:00:00 timestamp
+                for i in range(len(DATA["timestamp"])):
+                    if DATA["timestamp"][i][11:19] == '00:00:00':
+                        start_index = i
+                        break
+                data = DATA["activity"][start_index:]
 
 
-            # Segment the data into days and remove days with consecutive repeats and low activity
-            data_seg_days = []
-            num_segments = len(data) // 1440  # Number of segments for this file
-            for i in range(num_segments):
-                start_index = i * 1440
-                end_index = start_index + 1440
-                day_segments = data.iloc[start_index:end_index]
-                if day_segments.mean() > 10 and check_consecutive_repeats(day_segments, 200) == False: 
-                    data_seg_days.append(day_segments)
+                # Segment the data into days and remove days with consecutive repeats and low activity
+                data_seg_days = []
+                num_segments = len(data) // 1440  # Number of segments for this file
+                for i in range(num_segments):
+                    start_index = i * 1440
+                    end_index = start_index + 1440
+                    day_segments = data.iloc[start_index:end_index]
+                    if day_segments.mean() > 10 and check_consecutive_repeats(day_segments, 200) == False:
+                        data_seg_days.append(day_segments)
 
-            clean_df = pd.concat(data_seg_days, axis=0, ignore_index=True)
-            
+                clean_df = pd.concat(data_seg_days, axis=0, ignore_index=True)
 
-            # Segment the signal and add to the segmented_data list
-            num_segments = len(clean_df) // segment_length  # Number of segments in the clean dataframe
-            for i in range(num_segments):
-                start_index = i * segment_length
-                end_index = start_index + segment_length
-                segment = clean_df.iloc[start_index:end_index].values
-                
-                # SQUEEZING SEGMENTS
-                new_segment = []
 
-                for i in range(len(segment)//num_min):
-                    start_index = i * num_min
-                    end_index = start_index + num_min
-                    new_segment.append(sum(segment[start_index:end_index]))
+                # Segment the signal and add to the segmented_data list
+                num_segments = len(clean_df) // segment_length  # Number of segments in the clean dataframe
+                for i in range(num_segments):
+                    start_index = i * segment_length
+                    end_index = start_index + segment_length
+                    segment = clean_df.iloc[start_index:end_index].values
 
-                # ZERO PADDING
-                if len(new_segment) < TSlength_aligneds[j]:
-                    zero_pads = TSlength_aligneds[j] - len(new_segment)
-                    for _ in range(zero_pads): new_segment.append(0)
+                    # SQUEEZING SEGMENTS
+                    new_segment = []
 
-                segmented_data.append(new_segment)
-                labels.append(label)
+                    for i in range(len(segment)//num_min):
+                        start_index = i * num_min
+                        end_index = start_index + num_min
+                        new_segment.append(sum(segment[start_index:end_index]))
+
+                    # ZERO PADDING
+                    if len(new_segment) < TSlength_aligneds[j]:
+                        zero_pads = TSlength_aligneds[j] - len(new_segment)
+                        for _ in range(zero_pads): new_segment.append(0)
+
+                    if file_type == file_types[0]:  # filetypes[0] = train
+                        segmented_data_train.append(new_segment)
+                        labels_train.append(label)
+                    else:
+                        segmented_data_test.append(new_segment)
+                        labels_test.append(label)
+
 
     # Data matrix with labels
-    segmented_matrix = np.vstack(segmented_data)
-    label_column = np.array(labels).reshape(-1, 1)
-    data_matrix = np.hstack((label_column, segmented_matrix))
+    segmented_matrix_train = np.vstack(segmented_data_train)
+    segmented_matrix_test = np.vstack(segmented_data_test)
+    label_train_column = np.array(labels_train).reshape(-1, 1)
+    label_test_column = np.array(labels_test).reshape(-1, 1)
+
+    data_matrix_train = np.hstack((label_train_column, segmented_matrix_train))
+    data_matrix_test = np.hstack((label_test_column, segmented_matrix_test))
 
     # Shuffle the segmented matrix with labels
-    np.random.seed(42)
-    np.random.shuffle(data_matrix)
-
-    # Split the data into train and remaining data
-    train_data, remaining_data = train_test_split(data_matrix, test_size=0.75, stratify=data_matrix[:, 0], random_state=42)
-
-    # Split the remaining data into validation and test
-    val_data, test_data = train_test_split(remaining_data, test_size=0.89, stratify=remaining_data[:, 0], random_state=42)
+    np.random.shuffle(data_matrix_train)
+    np.random.shuffle(data_matrix_test)
 
     # Separate the features and labels for train, validation, and test sets
-    train_features = train_data[:, 1:]
-    train_labels = train_data[:, 0]
-    val_features = val_data[:, 1:]
-    val_labels = val_data[:, 0]
-    test_features = test_data[:, 1:]
-    test_labels = test_data[:, 0]
+    train_features = data_matrix_train[:, 1:]
+    train_labels = data_matrix_train[:, 0]
+    test_features = data_matrix_test[:, 1:]
+    test_labels = data_matrix_test[:, 0]
 
     # Convert features and labels to Torch tensors
     train_samples_tensor = torch.tensor(train_features).unsqueeze(1)
     train_labels_tensor = torch.tensor(train_labels)
-    val_samples_tensor = torch.tensor(val_features).unsqueeze(1)
-    val_labels_tensor = torch.tensor(val_labels)
     test_samples_tensor = torch.tensor(test_features).unsqueeze(1)
     test_labels_tensor = torch.tensor(test_labels)
 
     # Create dictionaries for train, validation, and test data
     train_data_dict = {"samples": train_samples_tensor, "labels": train_labels_tensor}
-    val_data_dict = {"samples": val_samples_tensor, "labels": val_labels_tensor}
     test_data_dict = {"samples": test_samples_tensor, "labels": test_labels_tensor}
 
-    print(Depression_datasets[j],train_samples_tensor.shape, val_samples_tensor.shape, test_samples_tensor.shape)
+    print(Depression_datasets[j],train_samples_tensor.shape, test_samples_tensor.shape)
     # Save Torch tensors as PT files in new created folders
     if not os.path.exists(f"datasets\\{Depression_datasets[j]}"):
         os.mkdir(f"datasets\\{Depression_datasets[j]}")
     torch.save(train_data_dict, f'datasets\\{Depression_datasets[j]}\\train.pt')
-    torch.save(val_data_dict, f'datasets\\{Depression_datasets[j]}\\val.pt')
     torch.save(test_data_dict, f'datasets\\{Depression_datasets[j]}\\test.pt')
