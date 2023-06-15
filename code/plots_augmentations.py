39f80d471dc1752db54d2264f11a39118b4eab0b
s214601 <s214601@student.dtu.dk>
2023-06-15 19:23:20 +0200

    plot of own augmentations TD step trend fix
    Signed-off-by: s214601 <s214601@student.dtu.dk>

--- code/plots_augmentations.py
+++ code/plots_augmentations.py
@@ -5,16 +5,44 @@ import math
 from augmentations import *
 import torch.fft as fft
 
-
-sleep_data = torch.load("C:/Users/danie/TFC-pretraining/datasets/SleepEEG/train.pt")
+sleep_data = torch.load('datasets\\SleepEEG\\train.pt')
 x0 = sleep_data['samples'][:100]
-x0.shape
+print(x0.shape)
 
+x_reverse = reverse(x0)
+x_flip = flip(x0)
+x_spike = spike(x0, num_spikes=x0.shape[2]/10)
+x_slope = slope_trend(x0, max_stds=2)
+x_step = step_trend(x0, num_steps=2, max_step=x0.shape[2]/20)
+
+jit_fig, ax = plt.subplots(6)
+
+ax[0].plot(x0[0][0], linewidth=1)
+ax[0].set_title("Original Signal (TD)")
+
+ax[1].plot(x_reverse[0][0], linewidth=1)
+ax[1].set_title("Reverse (TD)")
+
+ax[2].plot(x_flip[0][0], linewidth=1)
+ax[2].set_title("Flip horizontal (TD)")
+
+ax[3].plot(x_spike[0][0], linewidth=1)
+ax[3].set_title("Spikes (TD)")
+
+ax[4].plot(x_slope[0][0], linewidth=1)
+ax[4].set_title("Slope trend (TD)")
+
+ax[5].plot(x_step[0][0], linewidth=1)
+ax[5].set_title("Step trend (TD)")
+
+jit_fig.tight_layout()
+plt.show()
+'''
 x_f = fft.fft(x0).abs()
 
 aug_add = add_frequency(x_f,0.2)
 aug_remove = remove_frequency(x_f,0.5)
-
+'''
 '''fig, ax = plt.subplots(3)
 ax[0].plot(x_f[0][0], linewidth=3)
 ax[0].set_title("Original Signal")
@@ -24,9 +52,7 @@ ax[2].plot(aug_remove, linewidth=3)
 ax[2].set_title("Remove frequency")
 fig.tight_layout()
 plt.show()'''
-
-
-
+'''
 ## Using the jitter-augmentation
 aug_jit = jitter(x0,10)
 
@@ -61,4 +87,4 @@ ax[7].plot(aug_remove[0][0], color = "darkslateblue", linewidth=2)
 ax[7].set_title("Remove frequency (FD)")
 jit_fig.tight_layout()
 plt.show()
-
+'''
